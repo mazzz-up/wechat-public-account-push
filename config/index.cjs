614bcc6589a304dcf59a108841a66f584eb729f3
mazzz-up <759337476@qq.com>
2024-07-13 22:34:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1cwE6avQvmwkzordY8dHFhJUYRY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'wn8k9aFcGiGtDcQGf5zXFxrp5SIoRDFVpzMUUT-GPnc',
+      useTemplateId: 'Zs3WXN7j4UurcKRQiqhLmVzzZbeplKLSSLCckR9T1PU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-02',
       festivals: [
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-07-31' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '马力奔',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o1cwE6Zx_ubDyYp2rIpGGiTfwDqE',
     }
   ],
 
